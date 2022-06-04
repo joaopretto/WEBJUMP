17ff60193c2c18f33b2bbe45ab1bb6ca0c1f40e9
João Pretto <joao.pretto99@hotmail.com>
2022-06-04 17:37:45 -0300

    Comandos Customizados

--- cypress/integration/webjump-automacao.spec.js
+++ cypress/integration/webjump-automacao.spec.js
@@ -4,35 +4,21 @@ describe('WEBJUMP! automation test.', function() {
     beforeEach(function(){
         cy.visit('https://wj-qa-automation-test.github.io/qa-test/');
     })
+
+    //Utilizei o Comando Customizado oferecido pelo Cypress, encontrado na pasta ../support/commands.js
+    
     it('must click the "One", "Two" and "Four" buttons and check the same.', function(){
-        cy.get('#panel_body_one #btn_one').click().should('not.be.visible');
-        cy.get('#panel_body_one #btn_two').click().should('not.be.visible');
-        cy.get('#panel_body_one #btn_link').click().should('not.be.visible');
+
+        cy.clickOneTwoFourButtonsAndValidateVisible();
     })
+
     it('must click the "One", "Two" and "Four" buttons of the iFrame and check the same.', function(){
-        cy.get('#iframe_panel_body > iframe')
-          .its('0.contentDocument.body')
-          .find('#btn_one')
-          .click()
-          .should('not.be.visible');
 
-        cy.get('#iframe_panel_body > iframe')
-          .its('0.contentDocument.body')
-          .find('#btn_two')
-          .click()
-          .should('not.be.visible');
-
-        cy.get('#iframe_panel_body > iframe')
-          .its('0.contentDocument.body')
-          .find('#btn_link')
-          .click()
-          .should('not.be.visible');
+        cy.iFrameClickOneTwoFourButtonsAndValidateVisible();
     })
+
     it('fill in the "YourFirstName" field, click on the "One" button, check the "OptionThree", select the "ExampleTwo", and validate the Selenium image.', function(){
-        cy.get('#form_group #first_name').type("João");
-        cy.get('#panel_body_one #btn_one').click();
-        cy.get('#opt_three').click();
-        cy.get('#select_box').select('ExampleTwo');
-        cy.get('img[alt="selenium"]').invoke('show').should('be.visible');
+
+        cy.fillFieldYourFirstNameClickOneCheckOptionThreeSelectExempleTwoValidateSelenium();
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,54 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('clickOneTwoFourButtonsAndValidateVisible', function(){
+
+    cy.get('#panel_body_one #btn_one')
+        .click()
+        .should('not.be.visible');
+
+    cy.get('#panel_body_one #btn_two')
+        .click()
+        .should('not.be.visible');
+
+    cy.get('#panel_body_one #btn_link')
+        .click()
+        .should('not.be.visible');
+})
+
+Cypress.Commands.add('iFrameClickOneTwoFourButtonsAndValidateVisible', function(){
+
+    cy.get('#iframe_panel_body > iframe')
+          .its('0.contentDocument.body')
+          .find('#btn_one')
+          .click()
+          .should('not.be.visible');
+
+    cy.get('#iframe_panel_body > iframe')
+          .its('0.contentDocument.body')
+          .find('#btn_two')
+          .click()
+          .should('not.be.visible');
+
+    cy.get('#iframe_panel_body > iframe')
+          .its('0.contentDocument.body')
+          .find('#btn_link')
+          .click()
+          .should('not.be.visible');
+})
+
+Cypress.Commands.add('fillFieldYourFirstNameClickOneCheckOptionThreeSelectExempleTwoValidateSelenium', function(){
+    
+    cy.get('#form_group #first_name')
+        .type("João");
+
+    cy.get('#panel_body_one #btn_one')
+        .click();
+
+    cy.get('#opt_three')
+        .click();
+
+    cy.get('#select_box')
+        .select('ExampleTwo');
+
+    cy.get('img[alt="selenium"]')
+        .invoke('show')
+        .should('be.visible');
+})
